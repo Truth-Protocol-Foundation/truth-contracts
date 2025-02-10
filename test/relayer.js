b21f3d6314c8f68a9adad8299984f5e481bdc354
fluorostani <glyn.dimond@aventus.io>
2025-02-10 12:08:45 +0000

    Create relayer.js

---
+++ test/relayer.js
@@ -0,0 +1,64 @@
+const {
+  createLowerProof,
+  createTreeAndPublishRoot,
+  deployTruthBridge,
+  deployTruthToken,
+  EMPTY_32_BYTES,
+  expect,
+  getAccounts,
+  getNumRequiredConfirmations,
+  getPermit,
+  getTransferAuthorization,
+  getUSDC,
+  init,
+  ONE_HUNDRED_BILLION,
+  ONE_USDC,
+  randomBytes32,
+  sendUSDC,
+  ZERO_ADDRESS
+} = require('./helper.js');
+
+let bridge, truth, usdc, owner, relayer, user, t2PubKey;
+
+describe('User Functions', async () => {
+  before(async () => {
+    const numAuthors = 6;
+    await init(numAuthors);
+    [owner, relayer, user] = getAccounts();
+    truth = await deployTruthToken(ONE_HUNDRED_BILLION, owner);
+    bridge = await deployTruthBridge(truth, owner);
+    usdc = await getUSDC();
+    t2PubKey = await bridge.deriveT2PublicKey(user.address);
+  });
+
+  context('Lifting USDC via relayer', async () => {
+    context('succeeds', async () => {
+      before(async () => {
+        await bridge.registerRelayer(relayer.address);
+      });
+
+      async function doOnRamp() {
+        const amount = 10n * ONE_USDC;
+        await sendUSDC(user, amount);
+        const permit = await getPermit(usdc, user, bridge, amount, ethers.MaxUint256);
+        const { gasPrice } = await ethers.provider.getFeeData();
+        const expectedFee = await bridge.onRampCost(gasPrice);
+        await expect(bridge.connect(relayer).completeOnRamp(amount, user.address, permit.v, permit.r, permit.s))
+          .to.emit(bridge, 'LogLiftedToPredictionMarket')
+          .withArgs(usdc.address, t2PubKey, amount - expectedFee);
+      }
+
+      it('in lifting tokens to the prediction market with a valid permit', async () => {
+        const relayerBalanceBefore = await ethers.provider.getBalance(relayer.address);
+        for (i = 0; i < 10; i++) {
+          await doOnRamp();
+        }
+        const relayerBalanceAfter = await ethers.provider.getBalance(relayer.address);
+        await bridge.connect(relayer).recoupCosts();
+        const relayerBalanceRecouped = await ethers.provider.getBalance(relayer.address);
+        console.log(relayerBalanceBefore - relayerBalanceAfter);
+        console.log(relayerBalanceBefore - relayerBalanceRecouped);
+      });
+    });
+  });
+});

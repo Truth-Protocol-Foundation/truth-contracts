f21118e31618e1c80de5ba1102da42f921a99457
fluorostani <glyn.dimond@aventus.io>
2025-02-24 13:29:43 +0000

    Update user.js

--- test/user.js
+++ test/user.js
@@ -113,6 +113,14 @@ describe('User Functions', async () => {
         );
       });
 
+      it('attempting to proxy lift tokens without supplying a T2 public key', async () => {
+        const permit = await getPermit(truth, owner, bridge, amount);
+        await expect(bridge.connect(user).proxyLift(truth.address, owner.address, EMPTY_BYTES_32, amount, permit.deadline, permit.v, permit.r, permit.s)).to.be.revertedWithCustomError(
+          bridge,
+          'InvalidT2Key'
+        );
+      });
+
       it('attempting to lift more tokens than the T2 limit', async () => {
         const MAX_LIFT_AMOUNT = 2n ** 128n - 1n;
         const hugeSupplyToken = await deployTruthToken(999999999999999999999999999999n, owner);
@@ -208,7 +216,7 @@ describe('User Functions', async () => {
   });
 
   context('Reentrancy prevention', async () => {
-    const reentryPoint = { ClaimLower: 0, Lift: 1, LiftPermit: 2, LiftProxy: 3, LiftPM: 4, LiftPMPermit: 5, LiftPMProxy: 6 };
+    const reentryPoint = { ClaimLower: 0, Lift: 1, PermitLift: 2, ProxyLift: 3, PredictionMarketLift: 4, PredictionMarketPermitLift: 5, PredictionMarketProxyLift: 6 };
     const amount = 100n;
     let reentrantToken;
 
@@ -230,27 +238,27 @@ describe('User Functions', async () => {
     });
 
     it('the lift with permit re-entrancy check is triggered correctly', async () => {
-      await reentrantToken.setReentryPoint(reentryPoint.LiftPermit);
+      await reentrantToken.setReentryPoint(reentryPoint.PermitLift);
       await expect(bridge.lift(reentrantToken.address, t2PubKey, amount)).to.be.revertedWithCustomError(bridge, 'ReentrancyGuardReentrantCall');
     });
 
     it('the proxy lift with permit re-entrancy check is triggered correctly', async () => {
-      await reentrantToken.setReentryPoint(reentryPoint.LiftProxy);
+      await reentrantToken.setReentryPoint(reentryPoint.ProxyLift);
       await expect(bridge.lift(reentrantToken.address, t2PubKey, amount)).to.be.revertedWithCustomError(bridge, 'ReentrancyGuardReentrantCall');
     });
 
     it('the predictionMarketLift re-entrancy check is triggered correctly', async () => {
-      await reentrantToken.setReentryPoint(reentryPoint.LiftPM);
+      await reentrantToken.setReentryPoint(reentryPoint.PredictionMarketLift);
       await expect(bridge.lift(reentrantToken.address, t2PubKey, amount)).to.be.revertedWithCustomError(bridge, 'ReentrancyGuardReentrantCall');
     });
 
     it('the predictionMarketLift with permit re-entrancy check is triggered correctly', async () => {
-      await reentrantToken.setReentryPoint(reentryPoint.LiftPMPermit);
+      await reentrantToken.setReentryPoint(reentryPoint.PredictionMarketPermitLift);
       await expect(bridge.lift(reentrantToken.address, t2PubKey, amount)).to.be.revertedWithCustomError(bridge, 'ReentrancyGuardReentrantCall');
     });
 
     it('the proxy predictionMarketLift with permit re-entrancy check is triggered correctly', async () => {
-      await reentrantToken.setReentryPoint(reentryPoint.LiftPMPermit);
+      await reentrantToken.setReentryPoint(reentryPoint.PredictionMarketProxyLift);
       await expect(bridge.lift(reentrantToken.address, t2PubKey, amount)).to.be.revertedWithCustomError(bridge, 'ReentrancyGuardReentrantCall');
     });
   });

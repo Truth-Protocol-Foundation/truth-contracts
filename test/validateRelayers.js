a79aae76ff272f24e9af40441be4abcd709f1c8b
fluorostani <glyn.dimond@aventus.io>
2025-03-13 15:38:24 +0000

    Update validateRelayers.js

--- test/validateRelayers.js
+++ test/validateRelayers.js
@@ -14,13 +14,15 @@ const {
 } = require('./helper.js');
 
 const ROUNDS = 100;
-const MAX_GAS_PRICE = Number(ethers.parseUnits('5', 'gwei'));
-const MIN_GAS_PRICE = Number(ethers.parseUnits('0.5', 'gwei'));
-const MAX_USDC_AMOUNT = Number(100n * ONE_USDC);
+const NUM_RELAYERS = 3;
+const DELTA_SMOOTHING = 0.2; // (0.1 = slow, 0.5 = fast)
+const MIN_GAS_PRICE = Number(ethers.parseUnits('1', 'gwei'));
+const MAX_GAS_PRICE = MIN_GAS_PRICE * 10;
 const MIN_USDC_AMOUNT = Number(5n * ONE_USDC);
+const MAX_USDC_AMOUNT = Number(100n * ONE_USDC);
 const LOG_TX_GAS = false;
-const LOG_ROUNDS = true;
-const RELAYER_BASE_BALANCE = ethers.parseEther('0.5');
+const LOG_DETAILS = false;
+const RELAYER_BASE_BALANCE = ethers.parseEther('0.33');
 
 describe('Relayer Validation and Tuning', async () => {
   let bridge, truth, usdc, weth, swapHelper;
@@ -30,6 +32,7 @@ describe('Relayer Validation and Tuning', async () => {
   let users = [];
   let relayers = [];
   let scale = 1;
+  let emaDelta;
 
   before(async () => {
     const network = await init(6);
@@ -42,7 +45,7 @@ describe('Relayer Validation and Tuning', async () => {
     weth = await getWETH();
     swapHelper = await deploySwapHelper();
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < NUM_RELAYERS; i++) {
       const relayer = ethers.Wallet.createRandom().connect(ethers.provider);
       await owner.sendTransaction({ to: relayer.address, value: RELAYER_BASE_BALANCE });
       await bridge.registerRelayer(relayer.address);
@@ -64,14 +67,17 @@ describe('Relayer Validation and Tuning', async () => {
 
     const balances = await Promise.all(relayers.map(async relayer => await ethers.provider.getBalance(relayer.address)));
     const deltas = balances.map(balance => Number(ethers.formatEther(balance - RELAYER_BASE_BALANCE)).toFixed(6));
+    const delta = deltas.reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+    emaDelta = emaDelta === undefined ? delta : DELTA_SMOOTHING * delta + (1 - DELTA_SMOOTHING) * emaDelta;
 
-    if (LOG_ROUNDS) {
-      console.log(`\nROUND ${round} - TXS ${txCt} - DELTA ${deltas.reduce((a, b) => parseFloat(a) + parseFloat(b), 0).toFixed(4)}`);
+    console.log(`ROUND ${round}  TXS: ${txCt}  DELTA: ${emaDelta.toFixed(4)}`);
+
+    if (LOG_DETAILS) {
       console.log(`\nBalances:  ${balances.map(balance => Number(ethers.formatEther(balance)).toFixed(4)).join(' ')}`);
       console.log(`1 USDC Chainlink:   ${Number(ethers.formatEther(feedPrice * 1000000n)).toFixed(8)} ETH`);
       console.log(`1 USDC Uniswap:     ${Number(ethers.formatEther((await swapHelper.currentPrice()) * 1000000n)).toFixed(8)} ETH`);
       console.log(
-        `WETH:USDC Swapper:  ${Number(ethers.formatEther(await weth.balanceOf(swapHelper.address))).toFixed(4)} ${(Number(await usdc.balanceOf(swapHelper.address)) / 1e6).toFixed(2)}\n`
+        `WETH:USDC Swapper:  ${Number(ethers.formatEther(await weth.balanceOf(swapHelper.address))).toFixed(4)} ${(Number(await usdc.balanceOf(swapHelper.address)) / 1e6).toFixed(2)}\n\n`
       );
     }
   }
